ac7fe94cd0a46e1eafd29626218da84aba8c0e08
Alex Martins Coelho <alexcoelho18@gmail.com>
2026-07-28 11:59:09 -0400

    fase 0 e semana 1: setup e variaveis

---
+++ fase1-semana1/exercicios.js
@@ -0,0 +1,20 @@
+let cidadeNatal = "Nhamundá";
+let anoAtual = 2026;
+let gostaDeFutebol = true;
+let universidade = "Ufam"
+
+console.log (cidadeNatal);
+console.log (anoAtual);
+console.log (gostaDeFutebol);
+console.log (universidade);
+
+let precoProduto = 50;
+let quantidade = 3;
+let total = precoProduto*quantidade;
+
+console.log (total);
+
+let nomeAluno = "Alex";
+let curso = "Programação";
+
+console.log (`${nomeAluno} está estudando ${curso}`);
---
+++ fase1-semana1/variaveis.js
@@ -0,0 +1,7 @@
+let nome = "Alex"
+let idade = 36
+let estaEstudando = true;
+
+console.log (nome);
+console.log (idade);
+console.log (estaEstudando);
