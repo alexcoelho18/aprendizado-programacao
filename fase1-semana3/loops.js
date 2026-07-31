3b2948728264a9574d42fc0e464395e5ece73278
Alex Martins Coelho <alexcoelho18@gmail.com>
2026-07-31 11:17:10 -0400

    semana 3: loops for, decrescente, passo customizado e combinado com if

---
+++ fase1-semana3/loops.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <=5; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i>=1; i--) {
+    console.log (i);
+}
+
+for (let i = 0; i<=20; i+=2) {
+    console.log(i);
+    
+}
+
+for (let i = 1; i<=20; i++) {
+    if (i % 3 === 0) { 
+        console.log (i)
+    }
+    
+}
